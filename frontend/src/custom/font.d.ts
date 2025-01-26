9e555e14b75b823da6ab104956dae10c3c51c4a3
Zhang Li Kevin <zhanglikevinofficial@gmail.com>
2025-01-27 02:32:05 +0800

    Preload optimisations

--- frontend/src/custom/font.d.ts
+++ frontend/src/custom/font.d.ts
@@ -1,4 +1,4 @@
-declare module '*.ttf' {
+declare module '*.woff2' {
     const content: string;
-    export default content;
+    export default src;
   }
--- frontend/src/theme/ColorMode.tsx
+++ frontend/src/theme/ColorMode.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useMemo, useState } from "react";
 import { createTheme, PaletteMode, ThemeProvider } from "@mui/material/styles";
-import Montserrat from "../font/static/Montserrat-Regular.ttf";
+import Montserrat from "../font/Montserrat-Regular.woff2";
 
 export const ColorModeContext = createContext({
     mode: "light",
@@ -74,7 +74,7 @@ const ColorModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }
                             fontStyle: "normal",
                             fontDisplay: "swap",
                             fontWeight: 400,
-                            src: `local('Montserrat'), local('Montserrat-Regular'), url(${Montserrat}) format('ttf')`,
+                            src: `local('Montserrat'), local('Montserrat-Regular'), url(${Montserrat}) format('woff2')`,
                             unicodeRange:
                                 "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
                         },
