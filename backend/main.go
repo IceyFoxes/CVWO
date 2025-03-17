ed71d112b2ad9277d920049da90deae769774dee
Kevin <114825557+IceyFoxes@users.noreply.github.com>
2025-03-17 21:22:51 +0800

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -19,7 +19,7 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		// Change to your deployment app URL if deployed
-		AllowOrigins:     []string{"http://localhost:3000", "https://cvwo2025.netlify.app"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://forum2025.netlify.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
